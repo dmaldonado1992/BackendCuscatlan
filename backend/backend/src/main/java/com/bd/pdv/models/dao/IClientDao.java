e44aef5afe65ef48f484b76681f1b9d8f4d491c1
JOSUE DANIEL MALDONADO RECINOS <jmaldonador4@miumg.edu.gt>
2024-12-08 21:47:47 -0600

    Validaciones
    Validaciones

--- backend/backend/src/main/java/com/bd/pdv/models/dao/IClientDao.java
+++
@@ -1,7 +0,0 @@
-package com.bd.pdv.models.dao;
-
-import com.bd.pdv.models.entity.Client;
-import org.springframework.data.repository.CrudRepository;
-
-public interface IClientDao extends CrudRepository<Client, Integer> {
-}
--- backend/backend/src/main/java/com/bd/pdv/services/IRoleService.java
+++
@@ -1,9 +0,0 @@
-package com.bd.pdv.services;
-
-import com.bd.pdv.models.entity.Role;
-
-import java.util.Optional;
-
-public interface IRoleService {
-    public Optional<Role> getByName(String name);
-}
